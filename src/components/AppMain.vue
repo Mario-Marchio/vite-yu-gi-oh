ef2e6379dce1ba6cb915a1a0d5041d3831a6908a
Mario-Marchio <mariomarchio1610@gmail.com>
2023-07-03 20:09:15 +0200

    added some change for the layout

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -9,9 +9,9 @@ export default {
 
 <template>
     <div class="container">
-        <div>
+        <div class="row row-cols-5 justify-content-center">
             <div v-for="pokemon in store.pokemons" :key="pokemon.id">
-                <img :src="pokemon.imageUrl" :alt="pokemon.name">
+                <img :src="pokemon.imageUrl" :alt="pokemon.name" class="img-fluid">
                 <h1>{{ pokemon.number }}</h1>
                 <h1>{{ pokemon.name }}</h1>
                 <h1>{{ pokemon.type1 }}</h1>
