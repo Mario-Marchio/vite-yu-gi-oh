1c914f8611a4c4d795df6bb6c70170a2db6f1830
Mario-Marchio <mariomarchio1610@gmail.com>
2023-07-03 20:00:02 +0200

    appMain done

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import axios from 'axios';
+const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons';
+import { store } from './components/store.js'
+import AppMain from './components/AppMain.vue'
+export default {
+  components: { AppMain },
+  created() {
+    axios.get(endpoint).then(res => {
+      store.pokemons = res.data.docs;
+    })
+  }
+};
+</script>
+
+<template>
+  <AppMain />
+</template>
+
+<style lang="scss">
+@use './assets/scss/style.scss';
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,26 @@
+<script>
+import { store } from '../components/store.js'
+export default {
+    data() {
+        return { store }
+    }
+};
+</script>
+
+<template>
+    <div class="container">
+        <div>
+            <div v-for="pokemon in store.pokemons" :key="pokemon.id">
+                <img :src="pokemon.imageUrl" :alt="pokemon.name">
+                <h1>{{ pokemon.number }}</h1>
+                <h1>{{ pokemon.name }}</h1>
+                <h1>{{ pokemon.type1 }}</h1>
+
+            </div>
+
+        </div>
+    </div>
+</template>
+
+
+<style></style>
