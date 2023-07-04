5f615424ef778f952cba894656caad6aa72307c6
Mario-Marchio <mariomarchio1610@gmail.com>
2023-07-05 00:01:06 +0200

    es done pt2

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,16 +1,52 @@
 <script>
-import { store } from '../components/store.js'
+
+import axios from 'axios';
+import SearchOption from './SearchOption.vue'
+const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons';
+import { store } from './store.js'
+
 export default {
+    components: { SearchOption },
     data() {
-        return { store }
-    }
-};
+
+        return {
+            filteredPokemons: []
+
+        }
+    },
+    created() {
+        axios.get(endpoint)
+            .then(res => {
+                store.pokemons = res.data.docs;
+                this.filteredPokemons = store.pokemons;
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    },
+    methods: {
+        filterPokemonsByType(selectedType) {
+            const apiUrl = selectedType ? `${endpoint}?eq[type1]=${selectedType}` : endpoint;
+
+            axios.get(apiUrl)
+                .then(res => {
+                    this.filteredPokemons = res.data.docs;
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        },
+    },
+
+}
+
 </script>
 
 <template>
     <div class="container">
+        <SearchOption @type-selected="filterPokemonsByType" />
         <div class="row row-cols-5 justify-content-center">
-            <div v-for="pokemon in store.pokemons" :key="pokemon.id">
+            <div v-for="pokemon in filteredPokemons" :key="pokemon.id">
                 <img :src="pokemon.imageUrl" :alt="pokemon.name" class="img-fluid">
                 <h1>{{ pokemon.number }}</h1>
                 <h1>{{ pokemon.name }}</h1>
---
+++ src/components/SearchOption.vue
@@ -0,0 +1,52 @@
+<script>
+export default {
+    data() {
+
+        return {
+            options: [
+                "Bug",
+                "Dark",
+                "Dragon",
+                "Electric",
+                "Fairy",
+                "Fighting",
+                "Fire",
+                "Flying",
+                "Ghost",
+                "Grass",
+                "Ground",
+                "Ice",
+                "Normal",
+                "Poison",
+                "Psychic",
+                "Rock",
+                "Steel",
+                "Water"
+            ],
+            selectedType: ""
+        }
+
+    },
+    methods: {
+        filterPokemons() {
+            this.$emit('type-selected', this.selectedType);
+        },
+    },
+
+}
+
+</script>
+
+
+<template>
+    <div class="search-option">
+        <select v-model="selectedType" @change="filterPokemons">
+            <option value="">Seleziona un tipo</option>
+            <option v-for="option in options" :value="option" :key="option">{{ option }}</option>
+        </select>
+    </div>
+</template>
+
+
+
+<style></style>
